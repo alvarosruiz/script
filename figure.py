78649ef93e536dd8bf4ee0ac1f107882fa356cdd
alvarosruiz <69242226+alvarosruiz@users.noreply.github.com>
2020-09-20 17:17:39 +0200

    Update figure.py

--- figure.py
+++ figure.py
@@ -1,5 +1,11 @@
+import matplotlib as np
+import matplotlib.pylab as np
 import numpy as np
 
 time=[0,10,300000];
 
-COM=0.5*sin(time);
+COM=0.5*np.sin(time);
+
+:
+
+fig, ax = plt.subplots(time,COM)
